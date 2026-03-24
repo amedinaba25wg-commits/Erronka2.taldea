1cbebf4a34cf7ac94fc6b1373b436f92d0e30ca0
amedinaba25wg-commits <amedinaba25wg@ikzubirimanteo.com>
2026-03-24 11:18:17 +0100

    Add files via upload

--- project_animaliak/untitled/src/Main.java
+++ project_animaliak/untitled/src/Main.java
@@ -1,9 +1,10 @@
+import Datuak.ErabiltzaileZerrenda;
 import Windows.SaioaHasiMenua;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        new SaioaHasiMenua();
+        new SaioaHasiMenua(new ErabiltzaileZerrenda());
     }
 }
--- project_animaliak/untitled/src/Windows/UserMenua.java
+++ project_animaliak/untitled/src/Windows/UserMenua.java
@@ -3,10 +3,33 @@ package Windows;
 import Objektuak.Erabiltzaileak.Erabiltzailea;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class UserMenua extends JFrame {
     private Erabiltzailea erabiltzailea;
-    UserMenua(Erabiltzailea erabiltzailea) {
+    UserMenua() {
         this.erabiltzailea = erabiltzailea;
+        setTitle("Zubiri Animaliak");
+        setSize(400, 400);
+        setResizable(false);
+        setLayout(new GridLayout(3, 3));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //Goian ezkerrean
+        JPanel profila = new JPanel();
+        profila.setSize(50, 50);
+        profila.setLayout(new BorderLayout());
+        ImageIcon argazkia = new ImageIcon("argazkiak/erabiltzailea.png");
+        JLabel argazkiaLabel = new JLabel(argazkia);
+        JLabel izena = new JLabel("erabiltzailea");
+        profila.add(argazkiaLabel, BorderLayout.NORTH);
+        profila.add(izena, BorderLayout.SOUTH);
+        add(profila);
+
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new UserMenua();
     }
 }
